6a565eb2d6e8a7bd4316bb9da6fce348836f505a
Tuychiev Mukhtorjon <mrtuychiev61@gmail.com>
2022-10-17 11:10:33 +0500

    Heroku uchun o`zgarishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&%mp@#_koqs$=(leco170gfw0#k#ugr(+hc8y4bsi9fy=knvk3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
